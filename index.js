cb8cc3fb74d2d64e76e207ad4bd3a5f060dd6545
Gabriel Tay <gabrieltaykl@gmail.com>
2018-08-23 23:57:55 +0800

    process config var

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const helmet = require('helmet');
 const debug = require('debug')('app:index');
 const hsRoutes = require('./routes/bgpRequest');
 const app = express();
+const aws = require('aws-sdk');
 
+let env = new aws.S3({
+	environment: process.env.NODE_ENV,
+});
+
+console.log(env);
 // Declaring express middleware
 app.use(express.json());
 
